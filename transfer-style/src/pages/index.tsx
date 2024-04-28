b5a801afda562ec2d95783730831008dfe69eb84
Sedi <sedi@gmail.com>
2024-04-28 20:42:45 +0330

    banner added

--- transfer-style/src/pages/index.tsx
+++ transfer-style/src/pages/index.tsx
@@ -1,10 +1,17 @@
-import { Section } from "@/components";
+import { Banner, Section } from "@/components";
 
 export default function Home() {
   
   return (
    
-    <Section>lskfjlasdjfalsdf</Section>
+    <Section>
+      <Banner
+        title={"Dont miss amazing grocery deals"}
+        subtitle={"Sign up for the daily newsletter"}
+        image={"/assets/images/fresh-apples.png"}
+        bgImage={"/assets/images/banner_bg.png"}
+      />
+    </Section>
      
   );
 }
