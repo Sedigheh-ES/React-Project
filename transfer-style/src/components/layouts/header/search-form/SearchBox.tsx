5ef878e986b7aa78b8ee9bc62e65c98a532116bc
Sedi <sedi@gmail.com>
2024-05-25 18:33:53 +0330

    search API completed

--- transfer-style/src/components/layouts/header/search-form/SearchBox.tsx
+++ transfer-style/src/components/layouts/header/search-form/SearchBox.tsx
@@ -1,9 +1,10 @@
 import { getAllProductApiCall } from "@/api/Product";
 import { IconBox } from "@/components/common";
+import useDebounce from "@/hooks/use-debounce";
 import { EntityType } from "@/types";
 import { ProductType } from "@/types/api/Product";
 import { useMutation } from "@tanstack/react-query";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 
 interface Props{
@@ -16,17 +17,33 @@ interface FormInput{
 
 interface filterData{
   $containsi: string,
-  title:string
+
 }
 
 export function SearchBox({inputClassName=''}:Props) {
   //TODO should implenet Search Form
   const [resultData, setResultData] = useState<Array<EntityType<ProductType>>>([]);
-  const { register, handleSubmit } = useForm();
-  const mutation = useMutation({ mutationFn: (data:filterData) => getAllProductApiCall({ filters: data }) });
-  const onSubmit = (data:FormInput) => {
-   
+  const { register, handleSubmit, watch } = useForm<FormInput>();
+  const mutation = useMutation({ mutationFn: (data: filterData) => getAllProductApiCall({ filters: data }) });
 
+
+
+  const search_text = watch('search_text'); 
+  useEffect(() => {
+    if (search_text)
+    {
+      
+      delay();
+         
+   }else{setResultData ([]) }
+   
+ },[search_text])
+
+
+ 
+  const onSubmit = (data: FormInput) => {
+    if (data.search_text.length <= 1)
+      return;
     mutation.mutate({
       title: {
         '$containsi': data.search_text
@@ -41,11 +58,13 @@ export function SearchBox({inputClassName=''}:Props) {
     });
 
   }
- console.log('response', resultData);
+ const delay = useDebounce(handleSubmit(onSubmit), 1000);
+ 
+
     return (
       <div className={"relative"}>
          <form name="search-form" onSubmit={handleSubmit(onSubmit)} action="#" method="post" className="flex items-center">
-                  <input type="text"  {...register('search_text')}  placeholder={`Search for items`}className={`text-xsmall text-gray-400 border-gray-300 w-full focus:outline-none  ${inputClassName}`} />
+                  <input type="text" autoComplete={"off"}  {...register('search_text')}  placeholder={`Search for items`}className={`text-xsmall text-gray-400 border-gray-300 w-full focus:outline-none  ${inputClassName}`} />
                 <button type="submit"><IconBox icon={"icon-search"} size={22} /></button>
         </form>
         {
